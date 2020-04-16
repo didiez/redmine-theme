584d0fd851e0dedc0d82d798d50e53fd8d857e3c
Diego Díez Ricondo <diegodiez.ddr@gmail.com>
2020-04-16 19:46:26 +0200

    más ajustes

--- javascripts/theme.js
+++ javascripts/theme.js
@@ -1,14 +1,12 @@
 jQuery.fx.off = true;
 
-document.addEventListener('DOMContentLoaded', function(){ 
-  if(document.querySelector('body.controller-issues')){
-    var filtersTable = document.querySelector("#query_form_content #filters-table");
-    if(!filtersTable.children.length){
-      var fieldset = filtersTable.parentElement.parentElement;
-      fieldset.classList.add("collapsed");
-      fieldset.querySelector('legend.icon.icon-expended').classList.remove('icon-expended');
-      fieldset.querySelector('legend.icon').classList.add('icon-collapsed');
-    }
+document.addEventListener('DOMContentLoaded', function(){
+  var filtersTable = document.querySelector("#query_form_content #filters-table");
+  if(filtersTable && !filtersTable.children.length){
+    var fieldset = filtersTable.parentElement.parentElement;
+    fieldset.classList.add("collapsed");
+    fieldset.querySelector('legend.icon.icon-expended').classList.remove('icon-expended');
+    fieldset.querySelector('legend.icon').classList.add('icon-collapsed');
   }
 }, false);
 
