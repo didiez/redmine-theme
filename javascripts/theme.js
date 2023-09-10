7ce79d26c481d4ffbdb8179eaf5a1a7e5b27b3f3
Diego Díez <diegodiez.ddr@gmail.com>
2023-09-10 13:10:48 +0200

    fix table filters detector

--- javascripts/theme.js
+++ javascripts/theme.js
@@ -1,13 +1,15 @@
 jQuery.fx.off = true;
 
 document.addEventListener('DOMContentLoaded', function(){
-  var filtersTable = document.querySelector("#query_form_content #filters-table");
-  if(filtersTable && !filtersTable.children.length){
-    var fieldset = filtersTable.parentElement.parentElement;
-    fieldset.classList.add("collapsed");
-    fieldset.querySelector('legend.icon.icon-expended').classList.remove('icon-expended');
-    fieldset.querySelector('legend.icon').classList.add('icon-collapsed');
-  }
+  setTimeout(function(){    
+    var filtersTable = document.querySelector("#query_form_content #filters-table");
+    if(filtersTable && !filtersTable.children.length){
+      var fieldset = filtersTable.parentElement.parentElement;
+      fieldset.classList.add("collapsed");
+      fieldset.querySelector('legend.icon.icon-expanded').classList.remove('icon-expanded');
+      fieldset.querySelector('legend.icon').classList.add('icon-collapsed');
+    }    
+  }, 1);
 }, false);
 
 
